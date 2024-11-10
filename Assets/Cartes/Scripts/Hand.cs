ab0d12fff2431a6e2d8f7b67cc0282e4456b7425
Tristan Cholette <tristancholette@gmail.com>
2024-11-10 14:50:49 -0500

    BCP de trucs card mechanics and scenes
    -----

--- Assets/Cartes/Scripts/Hand.cs
+++ Assets/Cartes/Scripts/Hand.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class Hand : MonoBehaviour
 {
-    private List<GameObject> hand = new();
+    public List<GameObject> hand = new();
     public float spacing = 0;
     public float rotFactor = 0;
     public float yBalancing = 0;
+    public float yHovered = 0;
+    public float scaleIncrease = 0;
+
+    public Deck deck;
 
     private void updateHand() {
         for (int index = 0; index < hand.Count; index++) {
             GameObject card = hand[index];
+            Drag drag = card.GetComponent<Drag>();
             float dynamicSpacing = spacing / hand.Count;
             float dynamicRot = rotFactor / hand.Count;
             float xPos = (transform.position.x + index * dynamicSpacing) - (hand.Count - 1) * dynamicSpacing / 2;
-            float yPos = (transform.position.y - Mathf.Abs(index - (hand.Count - 1)/2) * yBalancing);
-            card.transform.position = new Vector3(xPos, yPos, transform.position.z - index*0.1f);
+            float yPos;
+            float zPos;
+            if (drag.isHovering) {
+                yPos = (transform.position.y - yBalancing * Mathf.Abs(index - (-1.0f + hand.Count) / 2)) + yHovered;
+                card.transform.localScale = drag.initScale * scaleIncrease;
+                zPos = transform.position.z - hand.Count * 0.1f;
+            } else {
+                yPos = (transform.position.y - yBalancing * Mathf.Abs(index - (-1.0f + hand.Count)/ 2));
+                card.transform.localScale = drag.initScale;
+                zPos = transform.position.z - index * 0.1f;
+            }
+            card.transform.position = new Vector3(xPos, yPos, zPos);
             card.transform.eulerAngles = new Vector3(0f, 0f, index * dynamicRot - (hand.Count - 1) * dynamicRot / 2);
         }
     }
---
+++ Assets/Cartes/Scripts/TurnSystem.cs
@@ -0,0 +1,208 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnSystem : MonoBehaviour {
+    public static bool isPlayerTurn = false;
+
+    public Deck playerDeck;
+    public Deck enemyDeck;
+    public GameObject player;
+    public GameObject enemy;
+
+    public float modifier;
+
+    public bool playerShielded = false;
+    public bool enemyShielded = false;
+
+
+    public GameObject playerShield;
+    public GameObject enemyShield;
+
+    public int playerLife = 10;
+    public int enemyLife = 10;
+
+    public GameObject cardContainer;
+
+    static public bool lose = false;
+    static public bool win = false;
+
+
+    public void Start() {
+
+        int i = 0;
+
+        foreach (Card card in enemyDeck.deckCards) {
+            enemyDeck.deck.Add(card);
+        }
+
+        foreach (Card card in playerDeck.deckCards) {
+            playerDeck.deck.Add(card);
+        }
+
+        enemyDeck.DrawCard();
+        enemyDeck.DrawCard();
+        enemyDeck.DrawCard();
+        enemyDeck.DrawCard();
+
+        playerDeck.DrawCard();
+        playerDeck.DrawCard();
+
+
+        StartCoroutine(PlayBot());
+    }
+
+    private IEnumerator PlayBot() {
+        if (enemyShielded) {
+            enemyShield.SetActive(false);
+            enemyShielded = false;
+        }
+
+        yield return new WaitForSeconds(0.1f);
+
+        enemyDeck.DrawCard();
+
+        yield return new WaitForSeconds(1f);
+
+        int rand = Random.Range(0, enemyDeck.hand.hand.Count);
+        GameObject card = enemyDeck.hand.hand[rand];
+        Card stats = card.GetComponent<CardStyle>().cardStats;
+
+        enemyDeck.hand.RemoveCard(card);
+        card.GetComponent<CardStyle>().toggleFront(true);
+
+        card.transform.position += new Vector3(0f, 0f, -2f);
+
+        for (int i = 0; i < 10; i++) {
+            yield return new WaitForSeconds(0.05f);
+            card.transform.position += new Vector3(0f, -0.1f, 0f);
+        }
+
+        yield return new WaitForSeconds(1f);
+        cardContainer = card;
+        if (stats.givesDefense || stats.healing > stats.attack) {
+            enemyDeck.hand.RemoveCard(cardContainer);
+            Vector3 initPos = card.transform.position;
+            while (Vector3.Distance(card.transform.position,enemy.transform.position) > 1f) {
+                yield return new WaitForSeconds(0.05f);
+                card.transform.position += (enemy.transform.position - initPos).normalized;
+            }
+            StartCoroutine(HealEnemy(stats.healing));
+            StartCoroutine(ShieldEnemy(stats.givesDefense));
+
+        } else {
+            enemyDeck.hand.RemoveCard(cardContainer);
+            Vector3 initPos = card.transform.position;
+            while (Vector3.Distance(card.transform.position, player.transform.position) > 1f) {
+                yield return new WaitForSeconds(0.05f);
+                card.transform.position += (player.transform.position - initPos).normalized;
+            }
+            StartCoroutine(DamageSelf(stats.attack));
+
+        }
+
+        enemyDeck.PlaceBackCard(cardContainer);
+        yield return new WaitForSeconds(0.1f);
+
+        if (playerShielded) {
+            playerShield.SetActive(false);
+            playerShielded = false;
+        }
+
+        yield return new WaitForSeconds(0.1f);
+
+        playerDeck.DrawCard();
+        isPlayerTurn = true;
+    }
+
+    public IEnumerator UseCard(float xPos, GameObject cardObj) {
+        Card stats = cardObj.GetComponent<CardStyle>().cardStats;
+        cardContainer = cardObj;
+        yield return new WaitForEndOfFrame();
+        if (xPos > 960) {
+            Vector3 initPos = cardObj.transform.position;
+            while (Vector3.Distance(cardObj.transform.position, enemy.transform.position) > 1f) {
+                yield return new WaitForSeconds(0.05f);
+                cardObj.transform.position += (enemy.transform.position - initPos).normalized;
+            }
+            StartCoroutine(DamageEnemy(stats.attack));
+
+        } else {
+            Vector3 initPos = cardObj.transform.position;
+            while (Vector3.Distance(cardObj.transform.position, player.transform.position) > 1f) {
+                yield return new WaitForSeconds(0.05f);
+                cardObj.transform.position += (player.transform.position - initPos).normalized;
+            }
+            StartCoroutine(HealSelf(stats.healing));
+            StartCoroutine(ShieldSelf(stats.givesDefense));
+
+        }
+        StartCoroutine(PlayBot());
+        playerDeck.PlaceBackCard(cardContainer);
+    }
+
+    public IEnumerator DamageEnemy(int attack) {
+        Debug.Log("attackEnemy" + attack);
+        StartCoroutine(ChangeLifeEnemy(-1 * attack));
+        yield return new WaitForSeconds(2f);
+        Debug.Log("attacked");
+    }
+
+    public IEnumerator DamageSelf(int attack) {
+        Debug.Log("attackSelf");
+        StartCoroutine(ChangeLifeSelf(-1 * attack));
+        yield return new WaitForSeconds(2f);
+    }
+
+    public IEnumerator HealSelf(int healing) {
+        Debug.Log("HealSelf");
+        StartCoroutine(ChangeLifeSelf(healing));
+        yield return new WaitForSeconds(2f);
+    }
+
+    public IEnumerator HealEnemy(int healing) {
+        StartCoroutine(ChangeLifeEnemy(healing));
+        Debug.Log("HealEnemy");
+        yield return new WaitForSeconds(2f);
+    }
+    public IEnumerator ShieldSelf(bool canShield) {
+        if (canShield) {
+            yield return new WaitForSeconds(0.1f);
+            playerShield.SetActive(true);
+            playerShielded = true;
+            Debug.Log("ShieldSelf");
+        }
+    }
+    public IEnumerator ShieldEnemy(bool canShield) {
+        if (canShield) {
+            yield return new WaitForSeconds(0.1f);
+            enemyShield.SetActive(true);
+            enemyShielded = true;
+            Debug.Log("ShieldEnemy");
+        }
+    }
+
+
+    public IEnumerator ChangeLifeSelf(int life) {
+        if (!playerShielded) {
+            playerLife += life;
+            player.transform.localScale = new Vector2(playerLife * modifier, player.transform.localScale.y);
+            yield return new WaitForSeconds(2f);
+            if (playerLife <= 0) {
+                lose = true;
+            }
+        }
+    }
+
+    public IEnumerator ChangeLifeEnemy(int life) {
+        if (!enemyShielded) {
+            enemyLife += life;
+            enemy.transform.localScale = new Vector2(enemyLife * modifier, enemy.transform.localScale.y);
+            yield return new WaitForSeconds(2f);
+            if (enemyLife <= 0) {
+                win = true;
+            }
+        }
+    }
+
+}
